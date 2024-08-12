e33b6a4a758242b1255d605f68e27c250124fa41
Laky64 <iraci.matteo@gmail.com>
2024-08-12 16:01:39 +0200

    Fixed Regex Parsing

--- regex.go
+++ regex.go
@@ -5,5 +5,5 @@ import "regexp"
 var (
 	getFunctionInfoRgx = regexp.MustCompile(`([^(]+)\.(\(\S+\)\.)?([\w.]+)$`)
 	lambdaNameRgx      = regexp.MustCompile(`(^|\w+\.)func([0-9]+)`)
-	tagRgx             = regexp.MustCompile(`^(([[:lower:]^:]{2,10}): )?([\S \n]+)`)
+	tagRgx             = regexp.MustCompile(`^(([[:lower:]^:]{2,10}): )?(.+)`)
 )
