df18dc24a15e6968a5360265357c200b4f36eb67
Laky64 <iraci.matteo@gmail.com>
2024-08-14 02:39:12 +0200

    Updated Tests

--- log_test.go
+++ log_test.go
@@ -2,6 +2,7 @@ package gologging
 
 import (
 	"testing"
+	"time"
 )
 
 func TestStdLog(t *testing.T) {
@@ -10,4 +11,15 @@ func TestStdLog(t *testing.T) {
 	Warn("asasdf", 1)
 	Error("asasdf", 1)
 	Fatal("asasdf", 1)
+}
+
+func TestBrokenGoRoutine(t *testing.T) {
+	go Fatal("Error while sending message to Nick: Bad Request: message text is empty")
+	time.Sleep(1 * time.Second)
+}
+
+func TestLambda(t *testing.T) {
+	func() {
+		Fatal("Error while sending message to Nick: Bad Request: message text is empty")
+	}()
 }
